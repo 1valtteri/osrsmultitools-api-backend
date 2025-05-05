0bbcbc43fd2858d71e7107c94d91db65b242d985
1valtteri <kyllonen_35@hotmail.com>
2025-05-05 19:00:41 +0300

    Add files via upload

--- runescape-proxy-server/index.js
+++ runescape-proxy-server/index.js
@@ -10,37 +10,36 @@ app.use(cors());
 
 // fetch item data based on item id
 app.get("/item/:id", async (req, res) => {
-    const itemId = req.params.id;
+  const itemId = req.params.id;
 
-    try {
-        const response = await axios.get(
-            `https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=${itemId}`
-        );
-        res.json(response.data);
-    } catch (error) {
-        console.error("Error fetching item data: ", error.message);
-        res.status(500).json({error: "Failed to fetch item data"})
-    }
+  try {
+    const response = await axios.get(
+      `https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=${itemId}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching item data: ", error.message);
+    res.status(500).json({ error: "Failed to fetch item data" });
+  }
 });
 
 // fetch player data based on username
 app.get("/player/:username", async (req, res) => {
-    const username = req.params.username;
-    
-    try {
-        const response = await axios.get(
-            `https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=${username}`
-        );
-        res.json(response.data);
-    } catch (error) {
-        console.error("Error fetching player data: ", error.message);
-        res.status(500).json({error: "Failed to fetch player data"})
-    }
+  const username = req.params.username;
+
+  try {
+    const response = await axios.get(
+      `https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=${username}`
+    );
+    console.log(`Succesfully fetched player data: ${username}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching player data: ", error.message);
+    res.status(500).json({ error: "Failed to fetch player data" });
+  }
 });
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
-
-
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
